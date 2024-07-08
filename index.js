907a92fa635cef1bf4ea8e7635207c43cc025cb3
Rehmanm13 <commanderata@gmail.com>
2024-07-09 04:46:18 +0500

    Fixed routing and configuration

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const axios = require('axios');
-const multer = require('multer');
-const fs = require('fs');
-const path = require('path');
 
 const promptRoutes = require('./api/routes/promptRoutes');
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -18,11 +14,6 @@ app.use('/chat', promptRoutes);
 
 app.get('/hello', (req, res) => {
     res.send('Hello World');
-});
-
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
 });
 
 app.listen(port, () => {
